321945e2b589b0056265200bbe1362c7875c2c36
Christof <bauer.christof@gmail.com>
2021-05-18 19:47:04 -0500

    Endemic filter. Added endangered status.

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,15 @@ import HashLoader from "react-spinners/HashLoader"
 
 const Cozumon = ({ initialData }) => {
 
-  const [data, setData] = useState(initialData.results)
+  const [allSpecies, setAllSpecies] = useState(initialData.results)
+  const [species, setSpecies] = useState(initialData.results)
   const [fetching, setFetching] = useState(false)
   const [currentPage, setCurrentPage] = useState(1)
   const [totalAmount, setTotalAmount] = useState(initialData.total_results)
   const [totalPages, setTotalPages] = useState(Math.ceil(initialData.total_results / initialData.per_page))
+  const [displayEndemic, setDisplayEndemic] = useState(false)
 
-  if (!data) {
+  if (!species) {
     return (
       <div>
         <h2>Sorry, error fetching data.</h2>
@@ -22,11 +24,11 @@ const Cozumon = ({ initialData }) => {
   const fetchData = async (mode) => {
     setFetching(true)
     const nextPage = mode ? currentPage + 1 : currentPage - 1
-    const req = await fetch(`https://api.inaturalist.org/v1/observations/species_counts?place_id=37612&page=${nextPage}`);
-    const newData = await req.json()
+    const req = await fetch(`https://api.inaturalist.org/v1/observations/species_counts?place_id=37612&locale=en&page=${nextPage}`)
+    const newSpecies = await req.json()
     setCurrentPage(nextPage)
     setFetching(false)
-    return setData(newData.results)
+    return setSpecies(newSpecies.results)
   }
 
   const paginate = (mode) => {
@@ -34,6 +36,7 @@ const Cozumon = ({ initialData }) => {
   }
 
   const Pagination = () => {
+    if (displayEndemic) return ``
     return (
       <div className="w-full flex justify-between">
         <div className="w-1/3">
@@ -55,6 +58,32 @@ const Cozumon = ({ initialData }) => {
     )
   }
 
+  const showEndemic = () => {
+    if (displayEndemic) {
+      setDisplayEndemic(false)
+      return setSpecies(allSpecies)
+    } else {
+      const newSpecies = species.filter(s => {
+        return s.taxon?.establishment_means?.establishment_means === "endemic"
+      })
+      setDisplayEndemic(true)
+      return setSpecies(newSpecies)
+    }
+  }
+
+  const getStatusName = (name) => {
+    switch (name) {
+      case "amenazada":
+        return "threatened"
+      case "sujeta a protección especial":
+        return "subject to special protection"
+      case "en peligro de extinción":
+        return "in danger of extinction"
+      default:
+        return name
+    }
+  }
+
   return (
     <div>
       <Head>
@@ -63,7 +92,7 @@ const Cozumon = ({ initialData }) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center px-8 w-screen">
+      <main className="flex flex-col items-center px-16 w-screen">
         <h1 className="text-4xl mt-4 mb-8 text-brand">Cozumel Taxonomy</h1>
 
         <div className="flex flex-col items-center w-full pb-16">
@@ -77,11 +106,16 @@ const Cozumon = ({ initialData }) => {
           </div>
 
           {!fetching &&
-            <ul>
+            <ul className="w-full">
+
+              <label htmlFor="endemic" className="mb-4 cursor-pointer">
+                <input type="checkbox" id="endemic" value={displayEndemic} onClick={showEndemic} />{` `}Show only endemic species
+              </label>
+
               <Pagination />
 
-              {data.map(d => {
-                const t = d.taxon
+              {species.map(s => {
+                const t = s.taxon
                 return (
                   <li key={t.id} className="bg-gray-50 shadow rounded p-6 my-6 flex space-x-6">
 
@@ -102,7 +136,10 @@ const Cozumon = ({ initialData }) => {
                       <h2 className="text-lg font-bold mb-4 text-brand">{t.preferred_common_name}</h2>
                       <p>Taxonomy: {t.iconic_taxon_name}</p>
                       <p>Latin: {t.name}</p>
-                      <p>Number of observations: {d.count}</p>
+                      <p>Number of observations: {s.count}</p>
+
+                      <p>Status: {t.conservation_status?.status_name ? getStatusName(t.conservation_status.status_name) : "no data"}</p>
+
                       {t.extinct ? <p>Species has become extinct...</p> : ``}
                     </div>
                   </li>
@@ -122,7 +159,7 @@ const Cozumon = ({ initialData }) => {
 
 export async function getServerSideProps(context) {
   // https://api.inaturalist.org/v1/observations?place_id=37612&per_page=200&order=desc&order_by=created_at
-  const url = `https://api.inaturalist.org/v1/observations/species_counts?place_id=37612`
+  const url = `https://api.inaturalist.org/v1/observations/species_counts?place_id=37612&locale=en`
   const res = await fetch(url)
   const data = await res.json()
 
