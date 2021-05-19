f86c3e09bd66405a4cff809325f00168d695a1e5
Christof <bauer.christof@gmail.com>
2021-05-19 14:33:25 -0500

    Enable filters.

--- pages/index.js
+++ pages/index.js
@@ -5,20 +5,22 @@ import HashLoader from "react-spinners/HashLoader"
 
 const Cozumon = ({ initialData }) => {
 
+  const speciesClasses = ['all', 'actinopterygii', 'animalia', 'amphibia', 'arachnida', 'aves', 'chromista', 'fungi', 'insecta', 'mammalia', 'mollusca', 'reptilia', 'plantae', 'protozoa', 'unknown']
   const [allSpecies, setAllSpecies] = useState(initialData.results)
   const [species, setSpecies] = useState(initialData.results)
-  const [fetching, setFetching] = useState(false)
-  const [currentPage, setCurrentPage] = useState(1)
   const [totalAmount, setTotalAmount] = useState(initialData.total_results)
+  const [currentPage, setCurrentPage] = useState(1)
   const [totalPages, setTotalPages] = useState(Math.ceil(initialData.total_results / initialData.per_page))
+  const [fetching, setFetching] = useState(false)
+  const [showFilters, setShowFilters] = useState(false)
+  const [selectedSpeciesClass, setSelectedSpeciesClass] = useState("all")
+  const [filteredSpeciesCount, setFilteredSpeciesCount] = useState(0)
+  const [filtered, setFilered] = useState(false)
   const [displayEndemic, setDisplayEndemic] = useState(false)
+  const [endemicSpeciesCount, setEndemicSpeciesCount] = useState(0)
 
   if (!species) {
-    return (
-      <div>
-        <h2>Sorry, error fetching data.</h2>
-      </div>
-    )
+    return <div><h2>Sorry, error fetching data.</h2></div>
   }
 
   const fetchData = async (mode) => {
@@ -36,12 +38,12 @@ const Cozumon = ({ initialData }) => {
   }
 
   const Pagination = () => {
-    if (displayEndemic) return ``
+    if (displayEndemic || filtered) return ``
     return (
-      <div className="w-full flex justify-between">
+      <div className="w-full flex justify-between mt-4">
         <div className="w-1/3">
           {currentPage > 1 &&
-            <svg xmlns="http://www.w3.org/2000/svg" onClick={() => paginate(0)} className="h-6 w-6 cursor-pointer hover:text-brand" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <svg xmlns="http://www.w3.org/2000/svg" onClick={() => paginate(0)} className="h-10 w-10 text-gray-600 cursor-pointer hover:text-brand" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
             </svg>
           }
@@ -49,7 +51,7 @@ const Cozumon = ({ initialData }) => {
         <p>Page {currentPage} / {totalPages}</p>
         <div className="w-1/3 flex justify-end">
           {currentPage < totalPages &&
-            <svg xmlns="http://www.w3.org/2000/svg" onClick={() => paginate(1)} className="h-6 w-6 cursor-pointer hover:text-brand" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <svg xmlns="http://www.w3.org/2000/svg" onClick={() => paginate(1)} className="h-10 w-10 text-gray-600 cursor-pointer hover:text-brand" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
             </svg>
           }
@@ -58,17 +60,34 @@ const Cozumon = ({ initialData }) => {
     )
   }
 
-  const showEndemic = () => {
+  const showEndemic = async () => {
     if (displayEndemic) {
       setDisplayEndemic(false)
       return setSpecies(allSpecies)
     } else {
-      const newSpecies = species.filter(s => {
-        return s.taxon?.establishment_means?.establishment_means === "endemic"
-      })
+      setFetching(true)
+      const req = await fetch(`https://api.inaturalist.org/v1/observations/species_counts?endemic=true&place_id=37612`)
+      const endemicSpecies = await req.json()
+      setEndemicSpeciesCount(endemicSpecies.total_results)
+      setFetching(false)
       setDisplayEndemic(true)
-      return setSpecies(newSpecies)
+      return setSpecies(endemicSpecies.results)
     }
+  }
+
+  const filterSpecies = async (identifier) => {
+    setSelectedSpeciesClass(identifier)
+    if (identifier === "all") {
+      setFilered(false)
+      return setSpecies(allSpecies)
+    }
+    setFetching(true)
+    const req = await fetch(`https://api.inaturalist.org/v1/observations/species_counts?place_id=37612&iconic_taxa=${identifier}`)
+    const filteredSpecies = await req.json()
+    setFilteredSpeciesCount(filteredSpecies.total_results)
+    setFilered(true)
+    setFetching(false)
+    return setSpecies(filteredSpecies.results)
   }
 
   const getStatusName = (name) => {
@@ -101,53 +120,91 @@ const Cozumon = ({ initialData }) => {
             <p className="text-xs mt-2">Total number of observed species: {totalAmount}</p>
           </h2>
 
-          <div className="mt-16">
-            <HashLoader color={"#556B2F"} loading={fetching} size={50} />
-          </div>
+          {fetching &&
+            <div className="mt-16">
+              <HashLoader color={"#556B2F"} loading={fetching} size={50} />
+            </div>
+          }
 
           {!fetching &&
-            <ul className="w-full">
+            <>
+              <a href="#" onClick={() => setShowFilters(!showFilters)} className="flex items-center w-max hover:text-brand mb-2">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4" />
+                </svg>
+                <span>Filter</span>
+              </a>
 
-              <label htmlFor="endemic" className="mb-4 cursor-pointer">
-                <input type="checkbox" id="endemic" value={displayEndemic} onClick={showEndemic} />{` `}Show only endemic species
-              </label>
+              {showFilters &&
+                <div className="border border-dashed p-6 mb-12">
+                  <p>Filter per taxanomy class:</p>
+                  <ul className="flex flex-wrap mt-2">
+                    {speciesClasses.map((c) => {
+                      // console.log(selectedSpeciesClass);
+                      return (
+                        <li key={c} className="w-32 mr-8 mt-1">
+                          <label htmlFor={c} className="block cursor-pointer">
+                            <input type="radio" id={c} checked={selectedSpeciesClass === c} onChange={() => filterSpecies(c)} />{` `}{c.charAt(0).toUpperCase() + c.slice(1)}
+                          </label>
+                        </li>
+                      )
+                    })
+                    }
+                  </ul>
+                  {selectedSpeciesClass === 'all' &&
+                    <label htmlFor="endemic" className="mt-4 block cursor-pointer">
+                      <input type="checkbox" id="endemic" checked={displayEndemic} onChange={showEndemic} />{` `}Show only endemic species
+                    </label>
+                  }
+                </div>
+              }
 
-              <Pagination />
+              <ul className="w-full">
 
-              {species.map(s => {
-                const t = s.taxon
-                return (
-                  <li key={t.id} className="bg-gray-50 shadow rounded p-6 my-6 flex space-x-6">
+                <Pagination />
 
-                    <div>
-                      {t.default_photo?.square_url ?
-                        <Image
-                          src={t.default_photo.square_url}
-                          height={100}
-                          width={100}
-                        />
-                        :
-                        <div className="w-24 h-24 text-sm flex items-center justify-center">No image</div>
-                      }
-                      <a href={t.wikipedia_url} target="_blank" className="block text-xs hover:text-brand">More information</a>
-                    </div>
+                {filtered &&
+                  <p><span className="font-bold">{filteredSpeciesCount}</span> species found in <span className="font-bold">{selectedSpeciesClass.charAt(0).toUpperCase() + selectedSpeciesClass.slice(1)}</span></p>
+                }
+                {displayEndemic && !filtered &&
+                  <p><span className="font-bold">{endemicSpeciesCount}</span> endemic species found</p>
+                }
 
-                    <div className="text-sm">
-                      <h2 className="text-lg font-bold mb-4 text-brand">{t.preferred_common_name}</h2>
-                      <p>Taxonomy: {t.iconic_taxon_name}</p>
-                      <p>Latin: {t.name}</p>
-                      <p>Number of observations: {s.count}</p>
+                {species.map(s => {
+                  const t = s.taxon
+                  return (
+                    <li key={t.id} className="bg-gray-50 shadow rounded p-6 my-6 flex space-x-6">
 
-                      <p>Status: {t.conservation_status?.status_name ? getStatusName(t.conservation_status.status_name) : "no data"}</p>
+                      <div>
+                        {t.default_photo?.square_url ?
+                          <Image
+                            src={t.default_photo.square_url}
+                            height={100}
+                            width={100}
+                          />
+                          :
+                          <div className="w-24 h-24 text-sm flex items-center justify-center">No image</div>
+                        }
+                        <a href={t.wikipedia_url} target="_blank" className="block text-xs hover:text-brand">More information</a>
+                      </div>
 
-                      {t.extinct ? <p>Species has become extinct...</p> : ``}
-                    </div>
-                  </li>
-                )
-              })}
+                      <div className="text-sm">
+                        <h2 className="text-lg font-bold mb-4 text-brand">{t.preferred_common_name}</h2>
+                        <p>Taxonomy: {t.iconic_taxon_name}</p>
+                        <p>Latin: {t.name}</p>
+                        <p>Number of observations: {s.count}</p>
 
-              <Pagination />
-            </ul>
+                        <p>Status: {t.conservation_status?.status_name ? getStatusName(t.conservation_status.status_name) : "no data"}</p>
+
+                        {t.extinct ? <p>Species has become extinct...</p> : ``}
+                      </div>
+                    </li>
+                  )
+                })}
+
+                <Pagination />
+              </ul>
+            </>
           }
 
         </div>
