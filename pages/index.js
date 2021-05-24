5ecfcbf6b5fbd16e6bf2d30e872c442e3d804ec2
Christof <bauer.christof@gmail.com>
2021-05-24 15:56:38 -0500

    Subpages ready

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import { useEffect, useState } from 'react'
 import HashLoader from "react-spinners/HashLoader"
 import translateStatusName from '../lib/translateStatusName'
 import downloadCsv from '../lib/downloadCsv'
 import capitalize from '../lib/capitalize'
 import scrollToTop from '../lib/scrollToTop'
+import Router from 'next/router'
 
 const Cozumon = ({ initialData }) => {
 
@@ -130,6 +132,11 @@ const Cozumon = ({ initialData }) => {
     setSpecies(allSpecies)
   }
 
+  const navigate = (e) => {
+    e.preventDefault()
+    Router.push(`/species/${e.target.dataset.id}`)
+  }
+
   return (
     <>
       <Head>
@@ -155,7 +162,6 @@ const Cozumon = ({ initialData }) => {
 
           {!fetching && !loading &&
             <>
-
               <form onSubmit={searchSpecies} className="flex items-center space-x-4">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
@@ -235,9 +241,8 @@ const Cozumon = ({ initialData }) => {
                   const { id, name, preferred_common_name, iconic_taxon_name, conservation_status, extinct, default_photo, wikipedia_url } = s.taxon
 
                   return (
-                    <li key={id + name + preferred_common_name} className="bg-gray-50 shadow rounded p-6 my-6 flex space-x-6">
-
-                      <div>
+                    <li onClick={navigate} data-id={id} key={id + name + preferred_common_name} className="bg-gray-50 shadow rounded p-6 my-6 flex space-x-6 cursor-pointer">
+                      <div className="pointer-events-none">
                         {default_photo?.square_url ?
                           <Image
                             src={default_photo.square_url}
@@ -247,12 +252,12 @@ const Cozumon = ({ initialData }) => {
                           :
                           <div className="w-24 h-24 text-sm flex items-center justify-center">No image</div>
                         }
-                        {wikipedia_url &&
-                          <a href={wikipedia_url} target="_blank" className="block text-xs hover:text-brand">More information</a>
-                        }
+                        {/* {wikipedia_url &&
+                          <a href={wikipedia_url} target="_blank" className="block text-xs hover:text-brand pointer-events-auto">More information</a>
+                        } */}
                       </div>
 
-                      <div className="text-sm">
+                      <div className="text-sm pointer-events-none">
                         <h2 className="text-lg font-bold mb-4 text-brand">{preferred_common_name ? capitalize(preferred_common_name) : "No common name available"}</h2>
                         <p>Latin Name: {name}</p>
                         <p>Taxonomy: {iconic_taxon_name}</p>
---
+++ pages/species/[id].js
@@ -0,0 +1,78 @@
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react';
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
+import translateStatusName from '../../lib/translateStatusName'
+import HashLoader from "react-spinners/HashLoader"
+
+const Species = () => {
+
+  const router = useRouter()
+  const identifier = router.query.id
+  const [species, setSpecies] = useState()
+
+  const fetchSpecies = async (identifier) => {
+    const url = `https://api.inaturalist.org/v1/taxa/${identifier}`
+    const res = await fetch(url)
+    const species = await res.json()
+    setSpecies(species.results[0].taxon_photos[0])
+  }
+
+  useEffect(() => {
+    if (identifier) fetchSpecies(identifier)
+  }, [identifier]);
+
+  if (!species) {
+    return (
+      <div className="mt-32 flex justify-center">
+        <HashLoader color={"#556B2F"} size={100} />
+      </div>
+    )
+  }
+
+  const { id, name, preferred_common_name, iconic_taxon_name, observations_count, extinct, wikipedia_url } = species.taxon
+
+  return (
+    <>
+
+      <Head>
+        <title>Cozumon</title>
+        <meta name="description" content="Cozumon | Cozumel Taxonomy" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="flex flex-col items-center px-16 w-screen">
+
+
+        <Link href="/"><a className="absolute left-4 top-8  border border-dotted p-2 hover:bg-brand hover:text-white transition-all">
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-12 w-12" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
+          </svg>
+        </a></Link>
+
+        <h1 className="text-4xl my-8 text-brand">{preferred_common_name}</h1>
+
+        <div className="flex flex-col items-center w-full pb-16">
+          <div className="shadow border border-dotted px-8 py-4 mb-8">
+            <p>Latin Name: {name}</p>
+            <p>Taxonomy: {iconic_taxon_name}</p>
+            <p>Number of observations: {observations_count}</p>
+            {/* <p>Status: {conservation_status?.status_name ? translateStatusName(conservation_status.status_name) : "no data"}</p> */}
+            {extinct ? <p>Species has become extinct...</p> : ``}
+            <a href={wikipedia_url} target="_blank" className="mt-4 text-sm block">More information</a>
+          </div>
+
+          <Image
+            src={species.photo.large_url}
+            width={species.photo.original_dimensions.width}
+            height={species.photo.original_dimensions.height}
+          />
+        </div>
+      </main>
+      {/* <pre>{JSON.stringify(species, null, 2)}</pre> */}
+    </>
+  )
+}
+
+export default Species
